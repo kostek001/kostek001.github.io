d2faeeb45da98cab2aa58f84c430e74c89726247
kostek0020 <kostek0020@gmail.com>
2023-01-20 01:12:45 +0100

    update

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ tl.add({
 
 
 /*-- PARTICLES --*/
-var numberOfEls = 200;
+var numberOfEls = 1000;
 var duration = 1000;
 var midScreenX = window.innerWidth / 2;
 var midScreenY = window.innerHeight / 2;
